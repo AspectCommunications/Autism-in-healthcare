4f969b587a458dd329246d44a4259fd5b2ec0217
AspectCommunications <comms@aspect.org.au>
2025-06-16 12:26:55 +1000

    Add files via upload

---
+++ story_content/5zADG2sNmVc_transcripts.js
@@ -0,0 +1,5 @@
+
+(function() {
+    const data = {"transcripts":[{"name":"captions","cues":[{"start":896,"text":"Autistic people often use stimming"},{"start":3296,"text":"which is short for self stimulatory behaviour"},{"start":6368,"text":"to regulate their senses. A stim could be rocking your body."},{"start":10880,"text":"It could be flapping your hands, could be spinning"},{"start":14208,"text":"or using a sensory item like this slinky that I've had hidden under the table."},{"start":20864,"text":"Stimming can be linked to emotions."},{"start":23007,"text":"And may increase or become more obvious when a person is excited,"},{"start":27200,"text":"anxious, happy or upset."}]}]};
+    window.globalLoadJsAsset('story_content/5zADG2sNmVc_transcripts.js', JSON.stringify(data));
+})();
---
+++ story_content/6MxhcIXhwii_transcripts.js
@@ -0,0 +1,5 @@
+
+(function() {
+    const data = {"transcripts":[{"name":"captions","cues":[{"start":96,"text":"The way an autistic person processes stimulation of these senses"},{"start":3460,"text":"can be more extreme than experienced by other people"},{"start":7244,"text":"For example, I call the hand dryers in the bathroom scream machines from  hell"},{"start":12992,"text":"because when those sounds go off I experience it as pain."},{"start":17144,"text":"It's like having somebody shove screwdrivers into my ears."}]}]};
+    window.globalLoadJsAsset('story_content/6MxhcIXhwii_transcripts.js', JSON.stringify(data));
+})();
